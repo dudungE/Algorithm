dc955e2240fbae240d05dfcef0d6dd16cc355211
JaeHyun Jeong <jjgus29@naver.com>
2025-03-23 19:25:45 +0900

    250323_update

---
+++ 문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        
+  
+        
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+            strings[i] = strings[i].charAt(n) + strings[i];
+        }
+        
+        Arrays.sort(strings);
+        
+        for (int i=0; i<strings.length; i++) {
+            strings[i] = strings[i].substring(1);
+        }
+        
+        
+        return strings;
+    }
+}
