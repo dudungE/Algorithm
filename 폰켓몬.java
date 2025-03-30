35147d4556316a0267f88b077a2fd4ece668c039
JaeHyun Jeong <jjgus29@naver.com>
2025-03-30 11:29:39 +0900

    250330_update

---
+++ 폰켓몬.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for (int i : nums) set.add(i);
+        return (set.size() > nums.length/2) ?  nums.length/2 : set.size();
+    }
+}
